44c4accd91864674b97bf52086f9e8f17937af28
Minh Kwang <mkw.dsa@gmail.com>
2025-12-31 14:48:55 +0000

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -14,7 +14,7 @@ echo "=================================================="
 echo ""
 echo "📋 STEP 1: Download Dataset"
 echo "--------------------------------------------------"
-python -m src.data.download_dataset
+python -m src.utils.download_dataset
 
 # Step 2: EDA
 echo ""
